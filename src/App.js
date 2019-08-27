b2c2c27998c2a8a4121a898ad167aa4b7155cfaf
Ricardo Villagrana <ricardovillagranal@gmail.com>
2019-08-26 20:51:56 -0500

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// eslint-disable-next-line
+import React from 'react'
+import './App.css'
+import './assets/style.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-row">
+      <div className="font-bold text-blue-500">
+        Hi everyone
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
